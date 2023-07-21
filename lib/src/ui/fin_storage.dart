3dcdf5d930748be1409ad60d0374981332b41dea
Junghyun An <daniel7806@naver.com>
2023-07-21 18:19:45 +0900

    add fin_storage

--- lib/src/ui/fin_storage.dart
+++ lib/src/ui/fin_storage.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:mobalworld/src/ui/storagebox_btn.dart';
 
-class FinStorage extends StatelessWidget {
+class FinStorage extends StatefulWidget {
   const FinStorage({Key? key}) : super(key: key);
 
+  @override
+  State<FinStorage> createState() => _FinStorageState();
+}
+
+class _FinStorageState extends State<FinStorage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,10 +43,22 @@ class FinStorage extends StatelessWidget {
       ),
       body: Column(
         children: [
-          SizedBox(height: 10,),
+          SizedBox(height: 0.05.sh,),
           Center(
             child:Column(
-
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 0.01.sh,
+                      vertical: 0.01.sh),
+                  child: Container(
+                    height: 0.15.sh,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.secondaryContainer,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                )
+              ],
             ),
           )
         ],

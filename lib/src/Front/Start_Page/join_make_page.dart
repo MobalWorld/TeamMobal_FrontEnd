e0ddd4609bd47c6b2e2b1cc0e252d78dcd7b581d
yeonhyeeun <0622yeon@naver.com>
2023-07-24 00:41:30 +0900

    👍🏻폰트 삭제

--- lib/src/Front/Start_Page/join_make_page.dart
+++ lib/src/Front/Start_Page/join_make_page.dart
@@ -33,18 +33,17 @@ class _JoinMakePageState extends State<JoinMakePage> {
                 Container(
                     child: Icon(
                   Icons.water_drop_outlined,
-                  size: 0.1.sh,
+                  size: 0.2.sh,
                   color: Color(0xFFFCCAA9),
                 )),
                 Container(
                   alignment: Alignment.bottomCenter,
                   child: const Text(
-                    '마음의 편지',
+                    '편지의 마음',
                     style: TextStyle(
                       color: Colors.black87,
                       fontSize: 30,
-                      fontWeight: FontWeight.w400,
-                      fontFamily: 'KOTRA HOPE',
+                      fontWeight: FontWeight.w500,
                     ),
                   ),
                 ),
@@ -73,7 +72,7 @@ class _JoinMakePageState extends State<JoinMakePage> {
                           Text(
                             '편지함을 만들고 싶나요?',
                             style: TextStyle(
-                                fontSize: 20, fontFamily: 'KOTRA HOPE'),
+                                fontSize: 20,),
                           ),
                           SizedBox(
                             height: 90,
@@ -96,7 +95,7 @@ class _JoinMakePageState extends State<JoinMakePage> {
                               style: TextStyle(
                                   color: Colors.black,
                                   fontWeight: FontWeight.w400,
-                                  fontFamily: 'KOTRA HOPE'),
+                                 ),
                             ),
                             style: TextButton.styleFrom(
                               backgroundColor: Color(0xFFFCCAA9),
@@ -111,62 +110,63 @@ class _JoinMakePageState extends State<JoinMakePage> {
                   height: 0.01.sh,
                 ),
                 Container(
-                  decoration: BoxDecoration(
-                    color: Color(0xFFFFF8E8),
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.2),
-                        spreadRadius: 5,
-                        blurRadius: 7,
-                        offset: Offset(0, 3), // changes position of shadow
-                      ),
-                    ],
-                  ),
-                  // 컨테이너의 차일드
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            '편지함을 주소를 알고 있나요?',
-                            style: TextStyle(
-                                fontSize: 20, fontFamily: 'KOTRA HOPE'),
-                          ),
-                          SizedBox(
-                            height: 90,
-                          )
-                        ],
-                      ),
-                      Container(
-                        child: Transform.translate(
-                          offset: Offset(0, 40),
-                          child: ElevatedButton(
-                            onPressed: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      //!!!이 아래 LoginPage()가 아니라 그룹 코드 작성하는 페이지로 이동해야함!!!
-                                      builder: (context) => LoginPage()));
-                            },
-                            child: Text(
-                              '참여하기',
+                    decoration: BoxDecoration(
+                      color: Color(0xFFFFF8E8),
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.2),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 3), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    // 컨테이너의 차일드
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '편지함을 주소를 알고 있나요?',
                               style: TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w400,
-                                  fontFamily: 'KOTRA HOPE'),
+                                  fontSize: 20,),
                             ),
-                            style: TextButton.styleFrom(
-                              backgroundColor: Color(0xFFFCCAA9),
+                            SizedBox(
+                              height: 90,
+                            )
+                          ],
+                        ),
+                        Container(
+                          child: Transform.translate(
+                            offset: Offset(0, 40),
+                            child: ElevatedButton(
+                              onPressed: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        //!!!이 아래 LoginPage()가 아니라 그룹 코드 작성하는 페이지로 이동해야함!!!
+                                        builder: (context) => LoginPage()));
+                              },
+                              child: Text(
+                                '참여하기',
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w400,
+                                   ),
+                              ),
+                              style: TextButton.styleFrom(
+                                backgroundColor: Color(0xFFFCCAA9),
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
+
                 MasterKey(margin: 5),
               ],
             ),

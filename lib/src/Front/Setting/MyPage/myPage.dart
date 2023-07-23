51d9417268d15d269e83df00808aa5ee86e0527f
yeonhyeeun <0622yeon@naver.com>
2023-07-23 23:54:02 +0900

    🌟다크모드 토글로 변경

--- lib/src/Front/Setting/MyPage/myPage.dart
+++ lib/src/Front/Setting/MyPage/myPage.dart
@@ -1,12 +1,13 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 
-import '../../Color_UI/padding.dart';
 import '../bottom.dart';
 import 'changeNickname.dart';
+
 
 class MyPage extends StatefulWidget {
   const MyPage({super.key});
@@ -83,7 +84,7 @@ class _MyPageState extends State<MyPage> {
                   },
                 )),
             Padding(
-              padding: GetPadding(),
+              padding: EdgeInsets.symmetric(horizontal: 0.02.sh),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -111,7 +112,7 @@ class _MyPageState extends State<MyPage> {
               ),
             ),
             Padding(
-              padding: GetPadding(),
+              padding: EdgeInsets.symmetric(horizontal: 0.02.sh),
               child: Divider(
                 height: 10,
                 color: Colors.black,
@@ -121,13 +122,12 @@ class _MyPageState extends State<MyPage> {
             Expanded(
               child: ListView(
                 physics: AlwaysScrollableScrollPhysics(),
-                
                 children: [
                   getSetting(hint: "계정 정보", nextPage: MyPage()),
                   getSetting(hint: "그룹 관리", nextPage: MyPage()),
-                  getSetting(hint: "다크 모드", nextPage: MyPage()),
                   getSetting(hint: "버전", nextPage: MyPage()),
                   getSetting(hint: "문의하기", nextPage: MyPage()),
+                  getDark(hint: "다크 모드", nextPage: MyPage()),
                   // Add more settings options as needed
                 ],
               ),
@@ -138,6 +138,24 @@ class _MyPageState extends State<MyPage> {
       bottomNavigationBar: bottomWidget(),
     );
   }
+
+  EdgeInsets GetPadding() => EdgeInsets.symmetric(vertical: 0.02.sh);
+
+  bool _lights = false; // 다크 모드 꺼져있음
+  //getDark 다크모드 토글 적용
+  Widget getDark({required String hint, required Widget nextPage}) {
+    return SwitchListTile(
+      title: const Text('다크 모드'),
+      value: _lights,
+      onChanged: (bool value) {
+        setState(() {
+          _lights = value;
+        });
+      },
+    );
+  }
+
+
 }
 
 ListTile getSetting({required String hint, required Widget nextPage}) {
@@ -152,3 +170,4 @@ ListTile getSetting({required String hint, required Widget nextPage}) {
     },
   );
 }
+

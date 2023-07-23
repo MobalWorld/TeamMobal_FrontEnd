9a0f2efaf16442b9ec90d1ce8e8d806b69411975
yeonhyeeun <0622yeon@naver.com>
2023-07-23 18:09:57 +0900

    🌿처음 조인 페이지에 패딩값 하나하나 준거 지움

--- lib/src/ui/join_make_page.dart
+++ lib/src/ui/join_make_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 
 //import 'package:mobalworld/src/ui/Setting/main_loading.dart';
@@ -21,162 +22,156 @@ class _JoinMakePageState extends State<JoinMakePage> {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 160,
-              ),
-              Container(
-                  child: Icon(
-                    Icons.water_drop_outlined,
-                    size: 60,
-                    color: Color(0xFFFCCAA9),
-                  )),
-              Container(
-                alignment: Alignment.bottomCenter,
-                child: const Text(
-                  '마음의 편지',
-                  style: TextStyle(
-                    color: Colors.black87,
-                    fontSize: 40,
-                    fontWeight: FontWeight.w400,
-                    fontFamily: 'KOTRA HOPE',
-                  ),
+          child: Padding(
+            // 전체 패딩 값 - 함수로 불러와서 쓰기
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 0.2.sh,
                 ),
-              ),
-              SizedBox(
-                height: 100,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Color(0xFFFFF8E8),
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.2),
-                        spreadRadius: 5,
-                        blurRadius: 7,
-                        offset: Offset(0, 3), // changes position of shadow
-                      ),
-                    ],
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(30),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              '편지함을 만들고 싶나요?',
-                              style: TextStyle(
-                                  fontSize: 20, fontFamily: 'KOTRA HOPE'),
-                            ),
-                            SizedBox(
-                              height: 90,
-                            )
-                          ],
-                        ),
-                        Container(
-                          child: Transform.translate(
-                            offset: Offset(0, 40),
-                            child: ElevatedButton(
-                              onPressed: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) =>
-                                            GoogleAdditionalPage()));
-                              },
-                              child: Text(
-                                '만들기',
-                                style: TextStyle(
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.w400,
-                                    fontFamily: 'KOTRA HOPE'),
-                              ),
-                              style: TextButton.styleFrom(
-                                backgroundColor: Color(0xFFFCCAA9),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
+                Container(
+                    child: Icon(
+                      Icons.water_drop_outlined,
+                      size: 0.1.sh,
+                      color: Color(0xFFFCCAA9),
+                    )),
+                Container(
+                  alignment: Alignment.bottomCenter,
+                  child: const Text(
+                    '마음의 편지',
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 30,
+                      fontWeight: FontWeight.w400,
+                      fontFamily: 'KOTRA HOPE',
                     ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Color(0xFFFFF8E8),
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.2),
-                        spreadRadius: 5,
-                        blurRadius: 7,
-                        offset: Offset(0, 3), // changes position of shadow
-                      ),
-                    ],
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(30),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              '편지함을 주소를 알고 있나요?',
-                              style: TextStyle(
-                                  fontSize: 20, fontFamily: 'KOTRA HOPE'),
-                            ),
-                            SizedBox(
-                              height: 90,
-                            )
-                          ],
+                SizedBox(
+                  height: 0.1.sh,
+                ),
+                 Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xFFFFF8E8),
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.2),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 3), // changes position of shadow
                         ),
-                        Container(
+                      ],
+                    ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                '편지함을 만들고 싶나요?',
+                                style: TextStyle(
+                                    fontSize: 20, fontFamily: 'KOTRA HOPE'),
+                              ),
+                              SizedBox(
+                                height: 90,
+                              )
+                            ],
+                          ),
+                          Container(
+                            child: Transform.translate(
+                              offset: Offset(0, 40),
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              GoogleAdditionalPage()));
+                                },
+                                child: Text(
+                                  '만들기',
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w400,
+                                      fontFamily: 'KOTRA HOPE'),
+                                ),
+                                style: TextButton.styleFrom(
+                                  backgroundColor: Color(0xFFFCCAA9),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                SizedBox(
+                  height: 0.01.sh,
+                ),
+                Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xFFFFF8E8),
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.2),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 3), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                  // 컨테이너의 차일드
+                  child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                '편지함을 주소를 알고 있나요?',
+                                style: TextStyle(
+                                    fontSize: 20, fontFamily: 'KOTRA HOPE'),
+                              ),
+                              SizedBox(
+                                height: 90,
+                              )
+                            ],
+                          ),
+                          Container(
+                            child: Transform.translate(
+                              offset: Offset(0, 40),
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        //!!!이 아래 LoginPage()가 아니라 그룹 코드 작성하는 페이지로 이동해야함!!!
+                                          builder: (context) => LoginPage()));
+                                },
+                                child: Text(
+                                  '참여하기',
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w400,
+                                      fontFamily: 'KOTRA HOPE'),
+                                ),
+                                style: TextButton.styleFrom(
+                                  backgroundColor: Color(0xFFFCCAA9),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
 
-                          child: Transform.translate(
-                            offset: Offset(0, 40),
-                            child: ElevatedButton(
-                              onPressed: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      //!!!이 아래 LoginPage()가 아니라 그룹 코드 작성하는 페이지로 이동해야함!!!
-                                        builder: (context) => LoginPage()));
-                              },
-                              child: Text(
-                                '참여하기',
-                                style: TextStyle(
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.w400,
-                                    fontFamily: 'KOTRA HOPE'),
-                              ),
-                              style: TextButton.styleFrom(
-                                backgroundColor: Color(0xFFFCCAA9),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
                     ),
-                  ),
-                ),
-              ),
-              MasterKey(margin: 5),
-            ],
+
+                MasterKey(margin: 5),
+              ],
+            ),
           ),
         ),
       ),
